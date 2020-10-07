f41464920ea14c68efb5b7d2be43f16bac4cfc4a
Mikhael Dantas <mikhael.dcs@gmail.com>
2020-10-06 21:45:44 -0300

    error handling tests

--- app/Exceptions/Handler.ts
+++ app/Exceptions/Handler.ts
@@ -20,4 +20,13 @@ export default class ExceptionHandler extends HttpExceptionHandler {
   constructor () {
     super(Logger)
   }
+
+  public async handle (error, ctx) {
+    if (error.code === 'E_VALIDATION_FAILURE') {
+      return ctx.response.status(422).send(error.messages)
+    }
+    // return ctx.response.status(500)
+
+    return super.handle(error, ctx)
+  }
 }
