6cfe9419593d8d8d6ebae63a0cf9a491f53549a8
Mikhael Dantas <mikhael.dcs@gmail.com>
2020-10-24 13:22:22 -0300

    updating logic for serviceorder creation

--- app/Controllers/Http/ServiceOrdersController.ts
+++ app/Controllers/Http/ServiceOrdersController.ts
@@ -7,7 +7,7 @@ import OrderStatus from 'App/Models/OrderStatus'
 import Product from 'App/Models/Product'
 import ServiceOrder from 'App/Models/ServiceOrder'
 import User from 'App/Models/User'
-import Wallet from 'App/Models/Wallet'
+import { PaymentStatusCodes } from 'App/utils/PaymentStatusCodes'
 
 export default class ServiceOrdersController {
   private returnOrderString (order:any) {
@@ -69,22 +69,20 @@ export default class ServiceOrdersController {
     const validatedData = await request.validate({schema: ApiValidSchemas.serviceorders.post})
 
     // start order creation
-    const errorSectionTrack = {
-      status: 0,
-      message: '',
-    }
+    const productErrorSectionTrack: {
+      id: number,
+      field: string,
+      message: string,
+    }[] = []
 
     await Database.transaction(async (trx) => {
-      // create order status
-      const orderStatus = new OrderStatus()
-      orderStatus.statusCode = 0
-      orderStatus.useTransaction(trx)
-
-      const orderStatusSaved = await orderStatus.save()
-
+      const orderStatus = await OrderStatus.findBy('status_code', PaymentStatusCodes.pending)
+      if (!orderStatus) {
+        throw new Error('no pending order status finded')
+      }
       // service order opening
       const serviceOrder = new ServiceOrder()
-      serviceOrder.orderStatusId = orderStatusSaved.id
+      serviceOrder.orderStatusId = orderStatus.id
       serviceOrder.userId = userId
       serviceOrder.useTransaction(trx)
 
@@ -95,8 +93,11 @@ export default class ServiceOrdersController {
       await Promise.all(validatedData.products.map(async orderedProductData => {
         const product = await Product.findBy('id', orderedProductData.product_id)
         if (!product) {
-          errorSectionTrack.status = 400
-          errorSectionTrack.message = `id in product with id ${orderedProductData.product_id}`
+          productErrorSectionTrack.push({
+            id: orderedProductData.product_id,
+            field: 'id',
+            message: 'product not finded',
+          })
           throw new Error('')
         }
         // increment totalvalue by the price times quantity
@@ -104,8 +105,11 @@ export default class ServiceOrdersController {
 
         // remove from stock
         if (product.stock_qty < orderedProductData.qty) {
-          errorSectionTrack.status = 400
-          errorSectionTrack.message = `qty in product with id ${orderedProductData.product_id}`
+          productErrorSectionTrack.push({
+            id: orderedProductData.product_id,
+            field: 'qty',
+            message: 'quantity unavailable',
+          })
           throw new Error('')
         }
 
@@ -129,42 +133,16 @@ export default class ServiceOrdersController {
         return orderedProduct
       }))
 
-      // Closing serviceOrder
+      // Closing serviceOrder and returning it
       savedServiceOrder.total_value = totalValue
       await savedServiceOrder.save()
 
-      // process payment
-      const wallet = await Wallet.findBy('user_id', userId)
-
-      if (!wallet) {
-        errorSectionTrack.status = 400
-        errorSectionTrack.message = 'wallet do not exist'
-        throw new Error('')
+      return savedServiceOrder
+    }).catch((error) => {
+      if (productErrorSectionTrack.length === 0) {
+        throw error
       }
-
-      if (wallet.money_qty < savedServiceOrder.total_value) {
-        errorSectionTrack.status = 400
-        errorSectionTrack.message = 'insuficient money'
-        throw new Error('')
-      }
-
-      wallet.money_qty -= savedServiceOrder.total_value
-
-      wallet.useTransaction(trx)
-
-      await wallet.save()
-
-      orderStatusSaved.statusCode = 1
-
-      orderStatusSaved.useTransaction(trx)
-      await orderStatusSaved.save()
-
-      response.status(200).send(savedServiceOrder)
-    }).catch(() => {
-      if (errorSectionTrack.status === 0) {
-        return response.status(500)
-      }
-      return response.status(errorSectionTrack.status).json({message: errorSectionTrack.message})
+      return response.status(400).json({errors: productErrorSectionTrack})
     })
   }
 }
